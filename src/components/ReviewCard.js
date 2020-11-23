f14ceef24246b29679a2e3a3d696a64aecd57221
skaz <nikkofann@gmail.com>
2020-11-23 13:27:04 +0200

    fixing reviews

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -3,13 +3,9 @@ export default function ReviewCard({ reviews }) {
     <div className="review-card-wrapper">
       <div className="mask">
         <ul>
-          {reviews.map((review, index) => {
-            return (
-              <li key={index} className={`review-card card${index}`}>
-                <img src={review}></img>
-              </li>
-            );
-          })}
+          <li key={index} className={`review-card card${index}`}>
+            <img src={review}></img>
+          </li>
         </ul>
       </div>
     </div>
